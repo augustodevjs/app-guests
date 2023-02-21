b18529c0f73b406b6a19d87dfadad3fa64f4f578
augusto <augustosaaude@gmail.com>
2023-02-21 11:10:15 -0300

    feat: adicionei listagem sem filtros

--- app/src/main/java/com/example/guests/repository/GuestRepository.kt
+++ app/src/main/java/com/example/guests/repository/GuestRepository.kt
@@ -7,6 +7,7 @@ import com.example.guests.model.GuestModel
 
 class GuestRepository private constructor(context: Context) {
     private val guestDataBase = GuestDataBase(context)
+
     companion object {
         private lateinit var repository: GuestRepository
 
@@ -50,7 +51,7 @@ class GuestRepository private constructor(context: Context) {
 
             db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
             true
-        } catch(e: Exception) {
+        } catch (e: Exception) {
             false
         }
     }
@@ -64,8 +65,46 @@ class GuestRepository private constructor(context: Context) {
 
             db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
             true
-        } catch(e: Exception) {
+        } catch (e: Exception) {
             false
         }
+    }
+
+    fun getAll(): List<GuestModel> {
+        val list = mutableListOf<GuestModel>()
+
+        try {
+            val db = guestDataBase.readableDatabase
+
+            val selection = arrayOf(
+                DataBaseConstants.GUEST.COLUMNS.ID,
+                DataBaseConstants.GUEST.COLUMNS.NAME,
+                DataBaseConstants.GUEST.COLUMNS.PRESENCE,
+            )
+
+            val cursor = db.query(
+                DataBaseConstants.GUEST.TABLE_NAME, selection, null, null, null, null, null
+            )
+
+            if (cursor != null && cursor.count > 0) {
+                while (cursor.moveToNext()) {
+                    val id =
+                        cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.ID))
+                    val name =
+                        cursor.getString(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.NAME))
+                    val presence =
+                        cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.PRESENCE))
+
+                    list.add(GuestModel(id, name, presence == 1))
+                }
+            }
+
+            cursor.close()
+
+        } catch (e: Exception) {
+            return list
+        }
+
+        return list
     }
 }
